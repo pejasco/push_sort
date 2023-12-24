c1c25a0a96eb5c4c7bc4a8acc9450c622d319139
Chung Fan Leung <chuleung@c3r1s4.42london.com>
2023-12-24 19:21:53 +0000

    haha

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: chuleung <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 18:12:10 by chuleung          #+#    #+#             */
-/*   Updated: 2023/11/13 19:38:25 by chuleung         ###   ########.fr       */
+/*   Updated: 2023/12/24 17:49:01 by chuleung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,17 +80,74 @@ char	**ft_split(char const *s, char c)
 	return (strs);
 }
 
+
+int		multi_strs_len(char **strs)
+{
+	int 	i;
+	int		j;
+	int		len;
+
+	i = 0;
+	len = 1;
+	while (strs[i])
+	{	
+		j = 0;
+		while(strs[i][j])
+		{
+			len++;
+			j++;
+		}
+		i++;
+	}
+	return (len);
+}
+
+char	*comb_multi_strs(char **strs)
+{
+	int		i;
+	int		j;
+	int		k;
+	int		len;
+	char	*full_str = NULL;
+
+	i = 0;
+	k = 0;
+	len = multi_strs_len(strs);
+	full_str = (char *)malloc(sizeof(int) * (len + 1));
+	while (strs[i])
+	{
+		j = 0;
+		while(strs[i][j] && k < len)
+		{
+				full_str[k] = strs[i][j];
+				k++;
+				j++;
+		}
+		i++;;
+	}
+	full_str[k] = '\0';
+	return (full_str);
+}
+
+
 #include <stdio.h>
 
 int	main(void)
 {
-	char *s = "./a.out 42 \"44 45 46\" 47 48";
-	char c1 = ' ';
-	char c2 = '"',
-	char **output;
+	char	*s = "./a.out 42 \"44 45 46\" 47 48";
+	char	c1 = '"';
+	char	c2 = ' ';
+	char	**output_del_c1;
+	char	*cat_output_del_c1;
+	char	**output_del_c1_n_c2;
 
-	output = ft_split(s, c1);
-	for (int i = 1; output[i] != NULL; i++)	
-		printf("%s\n", output[i]);
+	output_del_c1 = ft_split(s, c1);
+	cat_output_del_c1 = comb_multi_strs(output_del_c1);
+	output_del_c1_n_c2 = ft_split(cat_output_del_c1, c2);
+	for (int i = 1; output_del_c1_n_c2[i] != NULL; i++)
+		printf("%s\n", output_del_c1_n_c2[i]);
 	return (0);
 }
+
+
+
